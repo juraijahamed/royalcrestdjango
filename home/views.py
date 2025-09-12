25aa469db33bf2a6006bc69c663ec275d26a5908
royalcrest <royalcrestcbe@gmail.com>
2025-09-12 15:36:33 +0530

    edites views for os

--- home/views.py
+++ home/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from royalcrest import settings
 
 
 def home(request):
-    template_dir = settings.TEMPLATES[0]['DIRS']
-    files = os.listdir(template_dir[0])
-    return HttpResponse(f"Templates directory: {template_dir}<br>Files: {files}",{'active_page': 'home'})
+    return render(request, "home.html", {'active_page': 'home'})
 
 def about(request):
     return render(request, "about.html", {'active_page': 'about'})
