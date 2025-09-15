735c75db706f97b1db54ca62e0d4e915a2b403d4
royalcrest <royalcrestcbe@gmail.com>
2025-09-15 17:24:31 +0530

    added static files

--- royalcrest/settings.py
+++ royalcrest/settings.py
@@ -124,7 +124,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+# WhiteNoise for static file handling
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
