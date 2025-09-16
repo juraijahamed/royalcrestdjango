0c1557523078ecf308d474ec041e579429c697d0
royalcrest <royalcrestcbe@gmail.com>
2025-09-16 13:32:26 +0530

    added .env to vercelignore

--- royalcrest/settings.py
+++ royalcrest/settings.py
@@ -12,12 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 
-
-load_dotenv()
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
